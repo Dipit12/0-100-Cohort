e5b80f3b2967cfea3b1736950aad6a3cbdcc76d7
Dipit12 <dipitmadan@gmail.com>
2024-07-18 00:06:14 +0530

    updated with callback code

--- Week-1/JS-Foundations/functions.js
+++ Week-1/JS-Foundations/functions.js
@@ -5,3 +5,15 @@ function findSum(a,b){
 }
 
 console.log(findSum(3,2));
+// callbacks
+
+function findSquare(a,fnToCall){
+    let result  = a**2;
+    fnToCall(result);
+}
+
+function displayResult(data){
+    console.log("The result is: " + data)
+}
+
+findSquare(5,displayResult)
